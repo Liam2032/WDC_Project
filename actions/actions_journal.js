6eff846f6b6534a348e72924e1f077368c3d716f
Liam2032 <a1669892@student.adelaide.edu.au>
2017-03-29 22:21:12 +1030

    added sick as search

--- actions/actions_journal.js
+++ actions/actions_journal.js
@@ -1,22 +1,43 @@
 $(function() {
-	
+
     /* Search bar */
+    
     var $searchBar = $('#Search');
     var $btn = $('.btn');
 
     $btn.show();
-    $searchBar.hide();
-
     $btn.on('click', function(e) {
-        $(this).fadeOut(200);
-        $searchBar.fadeIn(400);
-        $searchBar.focus();
+        $btn.text('');
+        $btn.animate({
+            opacity: 0.2,
+            left: '+=130'
+        }, 300, function() {});
+        // $btn.fadeToggle();
+        // $btn.css("opacity", "0.3");
+        $searchBar.animate({
+            width: '+=160',
+            opacity: 1.0,
+        }, 300, function() {
+            $searchBar.focus();
+        });
     });
-    $searchBar.on('focusout', function(e) {
-        $(this).fadeOut(300);
-        $btn.fadeIn(200);
-        $searchBar.val('');
 
+    $searchBar.on('focusout', function(e) {
+        $btn.animate({
+            opacity: 1.0,
+            left: '-=130'
+        }, 300, function() {
+            $btn.text('Search');
+        });
+        $searchBar.animate({
+            width: '-=160',
+            opacity: 0.0,
+        }, 300, function() {
+            // $btn.fadeToggle();
+            $searchBar.val('');
+            // $('.btn').text('');
+
+        });
     });
 
     /* Sliding Menu */
@@ -25,6 +46,7 @@ $(function() {
     var $menu_toggle = $('#menu_toggle');
     var $menu_close = $('#close_menu');
     var $body = $('#calendar');
+
     $menu_toggle.on('click', function() {
         $menu.css("left", "0");
         $body.css("margin-left", "250px");
